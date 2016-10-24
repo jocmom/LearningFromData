51dbd1ccf62347996b7a7ebf2e6d70be2d8463a9
Mombach <jmo@nri.de>
2016-10-24 16:38:30 +0200

    Question 7 first draft

--- homework/bias_variance.py
+++ homework/bias_variance.py
@@ -1,15 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import random
 
-N = 10000
-x = np.random.uniform(-1,1,N)
-# reshape vector to (9,1) matrix
-X = x.reshape((-1,1))
-# g = 1.43*x
-f = np.sin(np.pi * x)
-
-def mse(x,y):
-    return (x-y)**2
+def target(x):
+    return np.sin(np.pi * x)
 
 def bias(g,f):
     return (g-f)**2
@@ -21,31 +15,55 @@ def linear_regression(X,y):
     X_dagger = np.linalg.pinv(X)
     return np.dot(X_dagger, y)
 
-w = []
-for i in range(-1,N-1,1):
-    points = np.array([[x[i]], [x[i+1]]])
-    targets = np.array([[f[i]], [f[i+1]]])
+if __name__ == "__main__":
+    N = 1000
+    # x = np.random.uniform(-1,1,N)
+    x = np.linspace(-1,1,N)
+    # reshape vector to (N,1) matrix
+    X = x.reshape((-1,1))
+    transform = lambda x,n: np.random.choice(x,n).reshape((-1,1))
+    w = np.ndarray(shape = (1, 0))
+    X = np.vstack([np.ones(N), x]).T
+    transform = lambda x,n: np.vstack([np.ones(n), np.random.choice(x,n)]).T
+    w = np.ndarray(shape = (2, 0))
+    # g = 1.43*x
+    f = np.sin(np.pi * x)
 
-    w.append(linear_regression(points, targets)[0])
-w = np.array(w)
-a_mean = np.mean(w)
-g_mean = a_mean*x
-print("a_mean: ", a_mean)
+    for i in range(N):
+        # points = np.array([[x[i]], [x[i+1]]])
+        points = transform(x, 2)
+        # point1 = np.ones(2).reshape((-1,1))
+        # point2 = np.random.choice(x, 2).reshape((-1,1))
+        # points = np.hstack([point1, point2])
+        """ get last column of target """
+        # w.append(linear_regression(points, target(points[:,[-1]])))
+        # print(w)
+        w = np.hstack([w, linear_regression(points, target(points[:,[-1]]))])
 
-b = bias(g_mean,f)
-print("bias: ", np.mean(b))
+    # print(w)
+    # w = np.array(w)
+    # print(w.T)
+    a_mean = np.mean(w)
+    g_mean = a_mean*x
+    print("a_mean: ", a_mean)
 
-variance = []
-hyp_set = np.dot(X, w.T)
-for h in hyp_set.T:
-    variance.append(np.mean(var(h,g_mean)))
-variance = np.mean(variance)
-print("var: ", variance)
+    b = bias(g_mean,f)
+    print("bias: ", np.mean(b))
+
+    variance = []
+    hyp_set = np.dot(X, w)
+    for h in hyp_set.T:
+        variance.append(np.mean(var(h,g_mean)))
+    variance = np.mean(variance)
+    print("var: ", variance)
 
 
-plt.scatter(x,b)
-plt.scatter(x,f)
-plt.scatter(x,g_mean)
-plt.show()
+    for h in hyp_set.T:
+        plt.plot(x,h,color='grey', alpha=0.1)
+    plt.plot(x,b,color='pink',label="bias")
+    plt.scatter(x,f,color='red',label="target")
+    plt.scatter(x,g_mean,label="g_mean")
+    plt.legend()
+    plt.show()
 
 
